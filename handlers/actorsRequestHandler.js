b358301c79e1ae7e70bd2a3a9519e9ca80d38b40
Ines Missoum <inesmiss@live.fr>
2019-10-28 18:24:24 +0100

    add actors handler

---
+++ handlers/actorsRequestHandler.js
@@ -0,0 +1,44 @@
+const {v3} = require('@leonardocabeza/the-movie-db');
+
+const v3Client = v3(process.env.API_KEY);
+
+const ActorsRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'ActorsIntent';
+    },
+    handle(handlerInput) {
+        let movie = handlerInput.requestEnvelope.request.intent.slots.nameMovie.value;
+
+        return v3Client.search.movies({query: movie})
+            .then((data) => {
+                let id = data.results[0].id;
+                return v3Client.movie.credits(id);
+
+            })
+            .then((credits) => {
+
+                let casting = credits.cast;
+                let actors = casting.map(actor => actor.name);
+
+                let responds = ["Here are a few actors that have played in this movie : " + actors[0] + ", " + actors[1] + ", " + actors[2],
+                    actors[0] + ", " + actors[1] + " and " + actors[2] + " played in this movie"];
+                let speakOutput = responds[Math.floor(Math.random() * responds.length)];
+                return handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .reprompt(speakOutput)
+                    .getResponse();
+            })
+            .catch(() => {
+                let speakOutput = "I don't know this movie.";
+                handlerInput.responseBuilder
+                    .speak(speakOutput)
+                    .reprompt(speakOutput)
+                    .getResponse();
+            })
+
+
+    }
+};
+
+module.exports = ActorsRequestHandler;
